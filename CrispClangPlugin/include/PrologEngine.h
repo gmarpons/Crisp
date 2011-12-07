720f3b2b43d491b2be790035af90efef5879eef0
Guillem Marpons <gmarpons@babel.ls.fi.upm.es>
2011-12-07 16:08:37 +0100

    GPL v3 license and copyright notice added to all source files.

--- CrispClangPlugin/include/PrologEngine.h
+++ CrispClangPlugin/include/PrologEngine.h
@@ -1,3 +1,22 @@
+// PrologEngine.h: Interface with the embbedded SWI Prolog engine. -*- C++ -*-
+//
+// Copyright (C) 2011 Guillem Marpons <gmarpons@babel.ls.fi.upm.es>
+//
+// This file is part of Crisp.
+//
+// Crisp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Crisp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Crisp.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <SWI-Prolog.h>
 #include <string>
 
@@ -11,4 +30,4 @@ namespace prolog {
   int plInteractiveSession();
   int plAssertDeclIsA(Decl *Decl, const std::string &Sort);
   int plAssertTypeIsA(Type *Type, const std::string &Sort);
-}
+} // End prolog namespace
