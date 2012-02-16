f97beb6fd1219a32a64d8f9c2c0ee4093bfd7946
Guillem Marpons <gmarpons@babel.ls.fi.upm.es>
2012-02-17 00:52:40 +0100

    Tentative implementation in Prolog of rule HICPP 3.4.2.

--- prolog/PrologBootForCrispLLVMPass/PrologBootForCrispLLVMPass.pl
+++ prolog/PrologBootForCrispLLVMPass/PrologBootForCrispLLVMPass.pl
@@ -18,3 +18,33 @@ llvmFactsFileName(ModuleFileName, PrologName) :-
 
 readAllViolationCandidates(FileName) :-
         ensure_loaded(FileName).
+
+%% FIXME: the following code doesn't belongs here
+
+violation('HICPP 3.4.2', [Func]) :-
+        violationCandidate('HICPP 3.4.2', [Func]),
+        hasArgument(Func, This), % Uses an iterator
+        hasName(This, "this"),
+        hasInstruction(Func, StoreThis), % Uses an iterator
+        isA(StoreThis, 'StoreInst'),
+        getValueOperand(StoreThis, This),
+        getPointerOperand(StoreThis, ThisAddr),
+        hasIntruction(Func, LoadThis),
+        isA(LoadThis, 'LoadInst'),
+        getPointerOperand(LoadThis, ThisAddr),
+        hasInstruction(Func, OffsetFromThis),
+        ( isA(OffsetFromThis, 'GetElementPtrInst'),
+          getPointerOperand(OffsetFromThis, LoadThis)
+        ; isA(OffsetFromThis, 'BitCastInst'),
+          hasOperand(OffsetFromThis, LoadThis) % Uses an iterator
+        ),
+                                % OffsetFromThis has necessarily
+                                % pointer type, so it has a location.
+        hasLocation(OffsetFromThis, OffsetFromThisLoc),
+        hasInstruction(Func, Return),
+        isA(Return, 'ReturnInst'),
+                                % That Func returns pointer (Clang
+                                % knows) guarantees there is a
+                                % location for 'Return'
+        hasLocation(Return, ReturnLoc),
+        aliasLessThan(ReturnLoc, OffsetFromThisLoc, 'NoAlias').
