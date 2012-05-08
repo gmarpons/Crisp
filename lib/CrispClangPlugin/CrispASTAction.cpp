28d3bc02a5c6612b24db6741768286ef7f833e93
Guillem Marpons <gmarpons@babel.ls.fi.upm.es>
2012-05-09 00:45:01 +0200

    Arg passing to clang plugin modified: separate options/inputs.

--- lib/CrispClangPlugin/CrispASTAction.cpp
+++ lib/CrispClangPlugin/CrispASTAction.cpp
@@ -24,6 +24,7 @@
 #include <vector>
 #include <boost/lambda/lambda.hpp>
 #include <boost/lambda/bind.hpp>
+#include <boost/range/algorithm/find_if.hpp>
 #include <boost/range/algorithm/for_each.hpp>
 #include <boost/range/iterator_range.hpp>
 
@@ -113,12 +114,9 @@ namespace crisp {
       DEBUG(dbgs() << "Main source file name: " << MainFileName << "\n");
 
       // Traverse types in the translation unit
-      b::for_each(b::make_iterator_range(Context.types_begin()
-                                         , Context.types_end())
-                  , l::bind(&CrispConsumer::VisitTypeFromTypesTable
-                            , this
-                            , l::_1)
-                  );
+      b::for_each(b::make_iterator_range(Context.types_begin(),
+                                         Context.types_end()),
+                  l::bind(&CrispConsumer::VisitTypeFromTypesTable,this,l::_1));
 
       // Traverse AST to visit declarations and statements
       TraverseDecl(Context.getTranslationUnitDecl());
@@ -171,31 +169,54 @@ namespace crisp {
 
   class CrispASTAction : public PluginASTAction {
   public:
-    CrispASTAction() {}
+    CrispASTAction() : FlagInteractive(false) {}
 
   protected:
-    virtual ASTConsumer* CreateASTConsumer(CompilerInstance& CI
-                                           , StringRef) {
+    virtual ASTConsumer* CreateASTConsumer(CompilerInstance &CI, StringRef) {
       return new CrispConsumer(CI, RulesFileName);
     }
 
-    virtual bool ParseArgs(const CompilerInstance &CI
-                           , const std::vector<std::string> &Args);
+    virtual bool ParseArgs(const CompilerInstance &CI,
+                           const std::vector<std::string> &Args);
   private:
     std::string RulesFileName;
+    bool FlagInteractive;
+    bool ParseOneArg(const std::string &);
   };
 
-  bool CrispASTAction::ParseArgs(const CompilerInstance &CI
-                                 , const std::vector<std::string> &Args) {
-    // One argument needed: rules file name.
-    if (Args.size() != 1) {
+  bool CrispASTAction::ParseArgs(const CompilerInstance &CI,
+                                 const std::vector<std::string> &Args) {
+
+    b::find_if(b::make_iterator_range(Args.begin(), Args.end()),
+               l::bind(&CrispASTAction::ParseOneArg, this, l::_1));
+
+    // At least one argument needed: rules file name.
+    if (RulesFileName.empty()) {
       DiagnosticsEngine &DE = CI.getDiagnostics();
       std::string DiagMsg = "rules file missing";
       unsigned DiagId = DE.getCustomDiagID(DiagnosticsEngine::Error, DiagMsg);
       DE.Report(DiagId);
       return false;
     }
-    RulesFileName = Args[0];
+    return true;
+  }
+
+  bool CrispASTAction::ParseOneArg(const std::string &Arg) {
+    if ( !Arg.empty()) {                  // Meaningfule argument
+      if (Arg[0] == '-') {                // Option argument
+        if (Arg.compare("--interactive") == 0) {
+          FlagInteractive = true;
+        } else {                          // Unknown option argument
+          return false;
+        }
+      } else {                            // Input argument
+        if ( !RulesFileName.empty()) {    // Rules file already set
+          return false;
+        } else {                          // Rules file name given
+          RulesFileName = Arg;
+        }
+      }
+    }
     return true;
   }
 
