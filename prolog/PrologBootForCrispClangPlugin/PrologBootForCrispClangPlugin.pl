7221cb43c51702dadfb8ac4e8cb38bb5f3651ce2
Guillem Marpons <gmarpons@babel.ls.fi.upm.es>
2012-05-15 20:49:05 +0200

    Add GPL v3 license and copyright notice to Prolog files.

--- prolog/PrologBootForCrispClangPlugin/PrologBootForCrispClangPlugin.pl
+++ prolog/PrologBootForCrispClangPlugin/PrologBootForCrispClangPlugin.pl
@@ -1,3 +1,22 @@
+%% PrologBootForCrispClangPlugin.pl -------------------------------*- Prolog -*-
+
+%% Copyright (C) 2011, 2012 Guillem Marpons <gmarpons@babel.ls.fi.upm.es>
+%%
+%% This file is part of Crisp.
+%%
+%% Crisp is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation, either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% Crisp is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with Crisp.  If not, see <http://www.gnu.org/licenses/>.
+
 :- multifile violation/3.
 :- multifile violation_candidate/2.
 :- multifile violation_llvm/3.
