5d1692333ce632c0e3b5dfa811e29fbe8f7f12ef
Guillem Marpons <gmarpons@babel.ls.fi.upm.es>
2012-05-16 17:16:42 +0200

    Makefile for examples dir, and small changes in example.

--- docs/examples/hicpp_3_3_13.cpp
+++ docs/examples/hicpp_3_3_13.cpp
@@ -4,6 +4,7 @@ public:
   B(B* other_b);
   virtual void func() {}
   virtual void func2() {}
+  virtual void func3() {}
   void non_virtual() { func(); }
 };
 
@@ -13,15 +14,17 @@ public:
   D(B* other_b) : B(other_b) {}
   virtual void func() {}
   virtual void func2() {}
+  virtual void func3() {}
 };
 
 B::B() {
   func();			// B::func called, not D::func
+  this->func2();		// B::func2 called, not D::func2
   non_virtual();		// B::func called, not D::func
 }
 
 B::B(B* other_b) {
-  other_b->func2();		// D::func2 called, on already
+  other_b->func3();		// D::func3 called, on already
                                 // constructed object
 }
 
